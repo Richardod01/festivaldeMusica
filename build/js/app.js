669d61e04ac7e398d3bb39eaf1a80ea812e6124d
Richardod01 <j.ricardo@live.com.mx>
2024-06-19 10:04:32 -0600

    Creacion de Modal y galeria finalizada

--- build/js/app.js
+++ build/js/app.js
@@ -3,5 +3,36 @@ document.addEventListener('DOMContentLoaded', function(){
 })
 
 function crearGaleria(){
+    const cantidadImagenes = 16;
     const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i=1; i<=cantidadImagenes; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src = `../src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galeria'
+
+        //even handler
+        imagen.onclick = function(){
+            mostrarImagen(i)
+        }
+
+        galeria.appendChild(imagen)
+    }
+}
+
+function mostrarImagen(i){
+    // Generar Modal
+
+    const modal = document.createElement('div')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    //agregar al HTML
+    const body = document.querySelector('body')
+    body.appendChild(modal)
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+    modal?.remove();
 }
